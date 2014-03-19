8823f5608a74acf9c186d43964806b25b0a9912e
ssusta20 <ssusta20@vm-0.ssusta20.koding.kd.io>
2014-03-19 09:20:20 +0000

    template.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Lead Management Login
+        </title>
+    </head>
+    <body>
+        <form>
+        Username:
+        <br>
+        Password:
+        </form>
+    </body>
+</html>
